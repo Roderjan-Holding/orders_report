a773dff31b75bb78013fe47d84dc64a52579bbc0
Peterson Paganelli <petersonroderjan@gmail.com>
2025-02-24 15:57:21 -0300

    [Feat]: build extract table by columns method

--- app/classes/HTMLExtractor.py
+++ app/classes/HTMLExtractor.py
@@ -1,4 +1,58 @@
 from app.classes.DatabaseHTMLExtractor import DatabaseHTMLExtractor
+from  bs4 import BeautifulSoup
+import html
 
 class HTMLExtractor(DatabaseHTMLExtractor):
-    pass
+    def __init__(
+        self,
+        queries,
+        databaseConnection
+    ) -> None:
+        super().__init__(
+            queries,
+            databaseConnection
+        )
+
+    def __extract_company_billing(self) -> str:
+        pass
+
+    def __extract_table_by_columns(self) -> dict:
+        columns = {}
+        html_data_list = self.get_html()
+
+        for html_data in html_data_list:
+            table = html_data.find("table")
+            if not table:
+                continue
+
+            #Extrair cabeçalho
+            headers = [th.get_text(strip=True) for th in table.find_all("th")]
+            if not headers:
+                first_row = table.find("tr")
+                if first_row:
+                    headers = [td.get_text(strip=True) for td in first_row.find_all("td")]
+            
+            #Adicionar cabeçalho no dicionário
+            for header in headers:
+                if header not in columns:
+                    columns[header] = []
+            
+            #Buscar todo conteúdo das células
+            rows = table.find_all("tr")[1:]
+            for row in rows:
+                cells = [td.get_text(strip=True) for td in row.find_all("td")]
+                if len(cells) == len(headers):
+                    for i, value in enumerate(cells):
+                        if value and value != "R$":
+                            columns[headers[i]].append(value)
+        if not columns:
+            return {"Error": "No table found in any HTML fragment."}
+            
+        return columns
+
+
+    def get_company_billing(self) -> str:
+        return self.__extract_company_billing()
+    
+    def get_table(self) -> dict:
+        return self.__extract_table_by_columns()
--- index.py
+++ index.py
@@ -1,15 +1,11 @@
-from app.classes.DatabaseHTMLExtractor import DatabaseHTMLExtractor
+from app.classes.HTMLExtractor import HTMLExtractor
 from app.config.config import DatabaseManager
 from app.sqls import sql_queries
 
 db_connection = DatabaseManager()
 
-decoded_html = DatabaseHTMLExtractor(
+decoded_html = HTMLExtractor(
     queries=sql_queries,
     databaseConnection=db_connection
 )
 
-decoded_html_list = decoded_html.get_html()
-
-for idx, html_content in enumerate(decoded_html_list, start=1):
-    print(f"HTML Content #{idx}:\n{html_content}\n{'-'*50}")
